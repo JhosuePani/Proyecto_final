457a85bc8171ef8a8db68310ec1e749e2cdbe52e
2jcortesj2 <jjose.cortes@udea.edu.co>
2023-05-25 18:57:12 -0500

    cambio de nombre de carpeta "vista" --> "imagenes"

--- vista.py
+++ vista.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import QMessageBox
 class Ventana(QtWidgets.QMainWindow): # La clase ventana herreda de QWitgets.QMainWindows
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self) #Estoy llamando al constructro de la clase que herede
-        uic.loadUi(r'Final/vistas/main_menu.ui', self) # estoy cargando el archivo de Qtdesign con el que voy a trabajar
+        uic.loadUi(r'vistas\main_menu.ui', self) # estoy cargando el archivo de Qtdesign con el que voy a trabajar
 
         # Vamos a conectar los botones
         #self.verifica = QtWidgets.QPushButton(self)
